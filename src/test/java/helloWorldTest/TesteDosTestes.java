b0d6a871f7f619ef77967cd87e74761e7ab955d8
Diego Ferreira <diego.ferreira.nti@ufpe.br>
2016-07-27 12:06:42 -0300

    configuracao dos testes

---
+++ src/test/java/helloWorldTest/TesteDosTestes.java
@@ -0,0 +1,25 @@
+package helloWorldTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ *  Classe responsavel por fazer a validacao 
+ *  do repositorio da classe {@link Disciplina}
+ *  
+ * @author Diego
+ *
+ */
+public class TesteDosTestes{
+		
+	/**
+	 * teste
+	 */
+	@Test
+	public void testeConsultaDisciplinaEspecifica(){
+		System.out.println("Funcionou!");
+		Assert.assertTrue(1 == 1);
+	}
+	
+}
